8acf6598da7be1ea17d32d453fdff88ae42153dd
irony-man <shivamrai0401@gmail.com>
2025-04-11 18:39:53 +0530

    Deployment to render build files added

--- reload.sh
+++ reload.sh
@@ -6,10 +6,11 @@ pip install -r requirements.txt
 # rm -rf db.sqlite3
 # ./manage.py makemigrations quora
 # ./manage.py migrate
+cd transport_simple
 cd quora/static_dev/quora
 npm i --legacy-peer-deps
 npm run build
 cd ../../../
-./manage.py collectstatic --no-input
-./manage.py migrate
+python3.13 manage.py collectstatic --no-input
+python3.13 manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.create_superuser('shivam', '', '123'); from common.models import UserProfile; UserProfile.objects.create(user=user)" | python manage.py shell
