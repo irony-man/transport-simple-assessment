f344a6fb6c16a712dcb643c01c1359dcdaf7e8bc
irony-man <shivamrai0401@gmail.com>
2025-04-11 18:43:07 +0530

    build command fixed

--- reload.sh
+++ reload.sh
@@ -11,6 +11,6 @@ cd quora/static_dev/quora
 npm i --legacy-peer-deps
 npm run build
 cd ../../../
-python3.13 manage.py collectstatic --no-input
-python3.13 manage.py migrate
+./manage.py collectstatic --no-input
+./manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.create_superuser('shivam', '', '123'); from common.models import UserProfile; UserProfile.objects.create(user=user)" | python manage.py shell
